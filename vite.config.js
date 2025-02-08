c26ff6c6076ba1356066cbfcda870a96e222390c
salwaaliakbar <salwaaliakbar22@gmail.com>
2025-02-08 20:05:23 +0500

    resolve .js and .jsx

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Learnify/', // Yeh line add karo
+  base: '/Learnify/',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 });
